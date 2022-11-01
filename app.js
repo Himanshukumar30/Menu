b6cc46249386ca0553206ee35f3309015d281559
Himanshu Kumar <himanshu.kumar2@outlook.com>
2022-11-01 08:32:18 -0700

    dynamically loaded categories on button click

--- app.js
+++ app.js
@@ -82,10 +82,11 @@ const menu = [
 ];
 // get parent element
 const sectionCenter = document.querySelector(".section-center");
-// const btnContainer = document.querySelector(".btn-container");
+const btnContainer = document.querySelector(".btn-container");
 // display all items when page loads
 window.addEventListener("DOMContentLoaded", function () {
   diplayMenuItems(menu);
+  displayMenuButtons();
 });
 
 function diplayMenuItems(menuItems) {
@@ -109,4 +110,44 @@ function diplayMenuItems(menuItems) {
   // console.log(displayMenu);
 
   sectionCenter.innerHTML = displayMenu;
+}
+function displayMenuButtons() {
+  const categories = menu.reduce(
+    function (values, item) {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ["all"]
+  );
+  const categoryBtns = categories
+    .map(function (category) {
+      return `<button type="button" class="filter-btn" data-id=${category}>
+          ${category}
+        </button>`;
+    })
+    .join("");
+
+  btnContainer.innerHTML = categoryBtns;
+  const filterBtns = btnContainer.querySelectorAll(".filter-btn");
+  console.log(filterBtns);
+
+  filterBtns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      // console.log(e.currentTarget.dataset);
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function (menuItem) {
+        // console.log(menuItem.category);
+        if (menuItem.category === category) {
+          return menuItem;
+        }
+      });
+      if (category === "all") {
+        diplayMenuItems(menu);
+      } else {
+        diplayMenuItems(menuCategory);
+      }
+    });
+  });
 }
